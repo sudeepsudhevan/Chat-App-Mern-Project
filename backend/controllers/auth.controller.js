96798d8c89892ead9e4881ebb2498880864ce92a
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-14 16:18:44 +0530

    Completed login User route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -55,8 +55,34 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    console.log('login');
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await User.findOne({ username });
+
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || '');
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({
+                error: 'Invalid username or password'
+            });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            message: 'Login successful',
+            _id: user._id,
+            username: user.username,
+            fullName: user.fullName,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in Login controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
 export const logout = (req, res) => {
