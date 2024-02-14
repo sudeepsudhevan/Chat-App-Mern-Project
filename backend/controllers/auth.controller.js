1ed1d4976325afeb8aee33be89d0ed0518bc83c8
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-14 16:26:16 +0530

    Completed Logout user endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -86,5 +86,13 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-    console.log('logout');
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({
+            message: 'Logout successful'
+        });
+    } catch (error) {
+        console.log("Error in Logout controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
