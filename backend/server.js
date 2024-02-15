57f0156f1738d06d9d947106920728a5cb399954
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-15 17:20:33 +0530

    Completed get user for side bar endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes.js';
-import connectToMongoDB from './db/connectToMongoDB.js';
-import messageRoutes from './routes/message.routes.js';
 import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
+import connectToMongoDB from './db/connectToMongoDB.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,8 +23,8 @@ app.use(cookieParser());
 // });
 
 app.use('/api/auth', authRoutes);
-
 app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
