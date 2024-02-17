af30797366c13b1cf281a1bc72c53ce15af0a4ec
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-17 15:51:07 +0530

    Completed Get conersation in sidebar

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,16 +1,26 @@
+import { useEffect } from "react"
+import useConversation from "../../zustand/useConversation"
 import MessageInput from "./MessageInput"
 import Messages from "./Messages"
 import { TiMessages } from 'react-icons/ti'
 
 const MessageContainer = () => {
-    const noChatSelected = true;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+
+    useEffect(() => {
+
+        // Reset the selected conversation when the component unmounts
+        return () => {
+            setSelectedConversation(null)
+        }
+    }, [setSelectedConversation])
     return (
         <div className="md:min-w-[450px] flex flex-col">
-            {noChatSelected ? <NoChatSelected /> : (
+            {!selectedConversation ? <NoChatSelected /> : (
                 <>
                     <div className="bg-slate-500 px-4 py-2 mb-2">
                         <span className="label-text">To:</span>{" "}
-                        <span className="text-gray-900 font-bold">John smith</span>
+                        <span className="text-gray-900 font-bold">{selectedConversation.fullName}</span>
                     </div>
 
                     <Messages />
