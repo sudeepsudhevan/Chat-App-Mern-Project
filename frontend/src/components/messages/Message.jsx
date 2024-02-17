e686236ab5ac479ab5f5e3a5eea6258fbef83528
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-17 18:53:43 +0530

    Completed getmessages functionality

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,22 +1,34 @@
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
 
-const Message = () => {
+const Message = ({ message }) => {
+    const { authUser } = useAuthContext();
+    const { selectedConversation } = useConversation()
+    const fromMe = message.senderId === authUser._id;
+    const formattedTime = extractTime(message.createdAt);
+    const chatClassName = fromMe ? "chat-end" : "chat-start";
+    const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+    const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+
     return (
-        <div className="chat chat-end">
+        <div className={`chat ${chatClassName}`}>
             <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
-                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="" />
+                    <img src={profilePic} alt="" />
                 </div>
             </div>
             {/* <div className="chat-header">
                 Anakin
                 <time className="text-xs opacity-50">12:46</time>
             </div> */}
-            <div className="chat-bubble text-white bg-blue-500">
-                Hello there!
+            <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
+                {message.message}
             </div>
             <div className="chat-footer opacity-50">
-                Seen at 12:46
+                {formattedTime}
             </div>
         </div>
     )
--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -11,8 +11,8 @@ const MessageInput = () => {
         if (!message) return;
         await sendMessage(message);
         setMessage("");
-
     }
+
     return (
         <form className="px-4 my-3" onSubmit={handleSubmit}>
             <div className="w-full relative">
