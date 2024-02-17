58710833b0ea3b10f218ce7a505f836824a85bee
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-17 12:33:37 +0530

    Added loading in signup page

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -81,7 +81,9 @@ const SignUp = () => {
                         Already have an account?
                     </Link>
                     <div>
-                        <button className="btn btn-block btn-sm mt-2 border border-slate-700">Sign Up</button>
+                        <button disabled={loading} className="btn btn-block btn-sm mt-2 border border-slate-700">
+                            {loading ? <span className="loading loading-spinner"></span> : "Sign Up"}
+                        </button>
                     </div>
                 </form>
 
